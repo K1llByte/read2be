b11b9bf41eb3a67555aec28fef2c3b7834281b82
KillByte <killbyte.prototype@gmail.com>
2021-03-07 17:35:07 +0000

    Added nodemon to backend dependencie

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -5,6 +5,18 @@ const auth = require('../controllers/auth');
 
 const router = express.Router();
 
+
+/**
+ * @swagger
+ * /users:
+ *  post:
+ *    summary: List users
+ *    description: List users
+ *    produces: application/json
+ *    responses:
+ *      '200':
+ *        description: Successful
+ */
 router.get('/users', (req, res) => {
     User.list_all()
     .then(usersdata => {
@@ -15,6 +27,20 @@ router.get('/users', (req, res) => {
     });
 });
 
+
+/**
+ * @swagger
+ * /users/{username}:
+ *  post:
+ *    summary: Get user
+ *    description: Get user by username
+ *    produces: application/json
+ *    responses:
+ *      '200':
+ *        description: Successful
+ *      '404':
+ *        description: Successful
+ */
 router.get('/users/:username', auth.authenticate(Permissions.Member), (req, res) => {
     User.get(req.params.username)
     .then(userdata => {
@@ -24,7 +50,7 @@ router.get('/users/:username', auth.authenticate(Permissions.Member), (req, res)
         }
         else
         {
-            res.status(400).json({ "error": "User doesn't exist" });
+            res.status(404).json({ "error": "User not found" });
         }
     })
     .catch(err => {
